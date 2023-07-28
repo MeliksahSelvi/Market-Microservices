3f18aaccf2522e1f9227a5c8ef8b4be6041b4444
mselvi <mselvi@uni-yaz.com>
2023-07-28 16:17:48 +0300

    -added DiscoveryServer module. This module task is maintain the service instances for service registry,unsubscribe actions.
    -added spring cloud dependencies for eureka client and eurea server.

---
+++ discovery-server/src/main/java/com/meliksah/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,18 @@
+package com.meliksah.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author mselvi
+ * @Created 28.07.2023
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
