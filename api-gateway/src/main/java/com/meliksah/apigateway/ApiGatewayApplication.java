d0e84cceb319a33436ac187d768d89445f0d77d5
mselvi <mselvi@uni-yaz.com>
2023-07-29 16:01:57 +0300

    -added spring cloud api gateway because this module will be taken for actions as routing service,security,loadbalancing.

---
+++ api-gateway/src/main/java/com/meliksah/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.meliksah.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author mselvi
+ * @Created 29.07.2023
+ */
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
