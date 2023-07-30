7cf67f54d77efe4ac419dee09eafd2da9df41796
mselvi <mselvi@uni-yaz.com>
2023-07-30 20:15:53 +0300

    - added spring boot actuator because we will trace to our app on actuator screen
    -added resillence4j library for implement circuit breakers pattern.
    
    -circuit breaker pattern include timelimiter,retryable features
    
    -added fallback method for placeOrder request. This method will invoke while circuit breaker state is open

--- order-service/src/main/java/com/meliksah/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/meliksah/orderservice/controller/OrderController.java
@@ -2,10 +2,16 @@ package com.meliksah.orderservice.controller;
 
 import com.meliksah.orderservice.dto.OrderRequestDto;
 import com.meliksah.orderservice.dto.OrderResponseDto;
+import com.meliksah.orderservice.dto.RestResponse;
 import com.meliksah.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @Author mselvi
@@ -20,9 +26,16 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
     @ResponseStatus(HttpStatus.CREATED)
-    public OrderResponseDto placeOrder(@RequestBody OrderRequestDto orderRequestDto){
+    public CompletableFuture<RestResponse> placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
         OrderResponseDto orderResponseDto = orderService.placeOrder(orderRequestDto);
-        return orderResponseDto;
+        return CompletableFuture.supplyAsync(()->RestResponse.of(orderResponseDto));
+    }
+
+    public CompletableFuture<RestResponse> placeOrderFallbackMethod(OrderRequestDto orderRequestDto, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(()->RestResponse.of("Ooops! Something went wrong, please order after some time!"));
     }
 }
