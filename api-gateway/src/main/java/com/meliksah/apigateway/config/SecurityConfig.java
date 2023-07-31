6f75d87484ae12dcdb73cca5d969e3eb9a24456d
mselvi <mselvi@uni-yaz.com>
2023-07-31 14:32:54 +0300

    -changed mongodb url because ex url is private.
    -refactor api gateway security config

--- api-gateway/src/main/java/com/meliksah/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/meliksah/apigateway/config/SecurityConfig.java
@@ -1,12 +1,9 @@
 package com.meliksah.apigateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
@@ -18,25 +15,17 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Value("${issuerUri}")
-    private String issuerUri;
-
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
 
 
         serverHttpSecurity.csrf().disable()
-                .authorizeExchange(exchange ->exchange
+                .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return serverHttpSecurity.build();
-    }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
     }
 }
