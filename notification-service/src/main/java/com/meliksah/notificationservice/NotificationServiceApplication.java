bd1583fa559fb53b657a913c9918e49fd245865d
mselvi <mselvi@uni-yaz.com>
2023-08-02 14:53:52 +0300

    -added notification service

---
+++ notification-service/src/main/java/com/meliksah/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,18 @@
+package com.meliksah.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author mselvi
+ * @Created 02.08.2023
+ */
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
