6020e8d5471a85c3f2221db4cda317d5fed4a246
mselvi <mselvi@uni-yaz.com>
2023-08-04 04:42:00 +0300

    -pushed these all microservices to docker hub repository
    -edited spring boot applications for Enable Discovery Client to Discovery Server

--- api-gateway/src/main/java/com/meliksah/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/meliksah/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.meliksah.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author mselvi
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
--- notification-service/src/main/java/com/meliksah/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/meliksah/notificationservice/NotificationServiceApplication.java
@@ -2,6 +2,7 @@ package com.meliksah.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author mselvi
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
